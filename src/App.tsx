9bf4cfdcbbf1cbcafba7f6bf970d135e6a357c55
puisci17-commits <puisci17@gmail.com>
2026-06-02 11:18:14 +0700

    feat: integrate Vercel Analytics
    Add @vercel/analytics to track site performance and user engagement.

--- src/App.tsx
+++ src/App.tsx
@@ -6,12 +6,14 @@
 import React from "react";
 import ResumeDeck from "./components/ResumeDeck";
 import DigitalTwinChat from "./components/DigitalTwinChat";
+import { Analytics } from "@vercel/analytics/react";
 
 export default function App() {
   return (
     <div className="relative min-h-screen">
       <ResumeDeck />
       <DigitalTwinChat />
+      <Analytics />
     </div>
   );
 }
